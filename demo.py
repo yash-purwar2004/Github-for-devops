3c15838c5927105fdc8a7ac31380d87473142334
yash-purwar2004 <yashpurwar2004@gmail.com>
2026-01-07 14:05:28 +0530

    New File added

---
+++ demo.py
@@ -0,0 +1,7 @@
+def my_func():
+    a = 9
+    b = 10
+
+    return d
+
+my_func()
--- test.py
+++ test.py
@@ -1,1 +1,1 @@
-print("If a person becomes a threat in your way than that person shoul be removed from his path first")
+print("BE A MENANCE TODAY, BE A PROBLEM")
